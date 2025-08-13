74045cf4f80aaa4fea6adaad6932e192a40c0de8
Lucas Colley <lucas.colley8@gmail.com>
2025-08-13 15:39:12 +0100

    add cpython [skip ci]

---
+++ cpython/recipe/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p ../build_dir
+cd ../build_dir
+
+if [[ -f configure-done ]]; then
+    echo "Skipping configure step, already done."
+else
+    $SRC_DIR/configure --prefix=${PREFIX} \
+        --oldincludedir=${BUILD_PREFIX}/${HOST}/sysroot/usr/include \
+        --enable-shared \
+        --srcdir=$SRC_DIR
+fi
+
+touch configure-done
+
+make -j${CPU_COUNT} install
